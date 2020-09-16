f64f57d3bd74acc6929fb546657e8ec6f8e1f05e
Maik Switalski <maik@zauberzeug.com>
2020-09-16 20:19:16 +0200

    first commit.

---
+++ buecherei/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  buecherei
+//
+//  Created by Maik Switalski on 16.09.20.
+//  Copyright © 2020 Maik Switalski. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World! asdfasdfs")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ buechereiTests/buechereiTests.swift
@@ -0,0 +1,34 @@
+//
+//  buechereiTests.swift
+//  buechereiTests
+//
+//  Created by Maik Switalski on 16.09.20.
+//  Copyright © 2020 Maik Switalski. All rights reserved.
+//
+
+import XCTest
+@testable import buecherei
+
+class buechereiTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
