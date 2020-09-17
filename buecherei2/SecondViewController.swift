5517b56b9c09771fb7aafe35c120c0cdc844289c
Maik Switalski <maik@zauberzeug.com>
2020-09-17 21:10:24 +0200

    use cocopod to use SwiftyJSON, store example json.

--- buecherei2/SecondViewController.swift
+++ buecherei2/SecondViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import Cocoa
+import SwiftyJSON
 
 class SecondViewController: NSViewController {
 
@@ -18,4 +19,14 @@ class SecondViewController: NSViewController {
     @IBAction func abbrechenGeklickt(_ sender: Any) {
         self.dismiss(self)
     }
+    @IBAction func speichern(_ sender: Any) {
+        print("[+] speichern wurde geklickt")
+        let levels = ["unlocked", "locked", "locked"]
+        let json = JSON(levels)
+        let str = json.description
+//        let data = str.dataUsingEncoding(NSUTF8StringEncoding)!
+        let data = str.data(using: String.Encoding.utf8)!
+        if let file = FileHandle(forWritingAtPath:"test.json") {
+            file.write(data)
+        }     }
 }
